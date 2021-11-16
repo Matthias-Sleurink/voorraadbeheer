e4d20414e97bf7bdf02bc4ff03d87d4360a942cd
Matthias-Sleurink <83409229+Matthias-Sleurink@users.noreply.github.com>
2021-11-16 19:24:51 +0100

    Keep running after any exception

--- client.py
+++ client.py
@@ -1,5 +1,13 @@
 import requests
 
+
+def main():
+    try:
+        while True:
+            requests.get(f"http://localhost:5000/scan/{input()}")
+    except Exception:
+        main()
+
+
 if __name__ == "__main__":
-    while True:
-        requests.get(f"http://localhost:5000/scan/{input()}")
+    main()
